879fcc7e15b716b683771a098861ba71e3ef9c1a
Asem Ashraf <asem.ashraf18@gmail.com>
2023-03-07 19:39:36 +0200

    Refactoring

--- AVR-ATMEGA32/HAL/SWITCH/SW_init.h
+++ AVR-ATMEGA32/HAL/SWITCH/SW_init.h
@@ -9,17 +9,17 @@
 // FUNCTIONS PROTOTYPES
 
 // Copy_pstructSwitches : pointer to the switches struct
-ES_t SW_enuInit(SW_t *SW_AstructSwitches);
+ES_t SW_enuInit      (SW_t *SW_AstructSwitches);
 
 
-// Copy_pstructSwitch : pointer to the switch struct
-// Copy_pu8State      : pointer to u8 variable to store the value of the switch
-ES_t SW_enuGetState(SW_t* Copy_pstructSwitch, u8 *pu8State);
+// Copy_pstructSwitch   : pointer to the switch struct
+// Copy_pu8State        : pointer to u8 variable to store the state of the switch
+ES_t SW_enuGetState  (SW_t* Copy_pstructSwitch, u8 *pu8State);
 
 
-// Copy_pstructSwitch : pointer to the switch struct
-// Copy_u8Mode        : DIO_u8FLOAT, DIO_u8PULL_UP
-ES_t SW_enuSetMode(SW_t* Copy_pstructSwitch, u8 Copy_u8Mode);
+// Copy_pstructSwitch   : pointer to the switch struct
+// Copy_u8Mode          : DIO_u8FLOAT, DIO_u8PULL_UP
+ES_t SW_enuSetMode   (SW_t* Copy_pstructSwitch, u8 Copy_u8Mode);
 
 
 #endif // SW_INIT_H

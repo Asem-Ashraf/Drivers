06b0bcba2ac1f6d9618d438d0e2a5759c411a1cd
Asem Ashraf <asem.ashraf18@gmail.com>
2023-03-07 19:39:56 +0200

    Added NULL pointer checking

--- AVR-ATMEGA32/HAL/SWITCH/SW_prog.c
+++ AVR-ATMEGA32/HAL/SWITCH/SW_prog.c
@@ -8,7 +8,8 @@
 
 // FUNCTIONS IMPLEMENTATION
 ES_t SW_enuInit(SW_t* Copy_AstructSwitchs){
-    ES_t Local_enuErrorState = ES_NOK;
+    ES_t Local_enuErrorState = ES_NULL_POINTER;
+    if (Copy_AstructSwitchs == NULL) { return Local_enuErrorState; }
     for (u8 i = 0; i<SW_u8NUM; i++) {
         Local_enuErrorState = DIO_enuSetPinDirection(Copy_AstructSwitchs[i].SW_u8PORTID,
                                                      Copy_AstructSwitchs[i].SW_u8PINID,
@@ -23,15 +24,20 @@ ES_t SW_enuInit(SW_t* Copy_AstructSwitchs){
     return Local_enuErrorState;
 }
 
+
 ES_t SW_enuGetState(SW_t* Copy_pstructSwitch, u8 *pu8State){
-    ES_t Local_enuErrorState = ES_NOK;
+    ES_t Local_enuErrorState = ES_NULL_POINTER;
+    if (Copy_pstructSwitch == NULL) { return Local_enuErrorState; }
     Local_enuErrorState = DIO_enuGetPinValue(Copy_pstructSwitch->SW_u8PORTID,
                                              Copy_pstructSwitch->SW_u8PINID,
                                              pu8State);
     return Local_enuErrorState;
 }
+
+
 ES_t SW_enuSetMode(SW_t* Copy_pstructSwitch, u8 Copy_u8Mode){
-    ES_t Local_enuErrorState = ES_NOK;
+    ES_t Local_enuErrorState = ES_NULL_POINTER;
+    if (Copy_pstructSwitch == NULL) { return Local_enuErrorState; }
     Local_enuErrorState = DIO_enuSetPinValue(Copy_pstructSwitch->SW_u8PORTID,
                                              Copy_pstructSwitch->SW_u8PINID,
                                              Copy_u8Mode);

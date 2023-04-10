9b23b99999f08ab3c8cd3f73a096d3b329082681
Asem Ashraf <asem.ashraf18@gmail.com>
2023-04-11 01:14:30 +0200

    Added Global Interrupt Driver

---
+++ AVR-ATMEGA32/MCAL/GIE/GIE_PROGRAM.c
@@ -0,0 +1,15 @@
+#include "../../LIB/stdTypes.h"
+#include "../../LIB/ErrorStates.h"
+
+
+ES_t GIE_enuEnable(void){
+    // Enable Global Interrupt Instruction
+    asm("SEI");
+    return ES_OK;
+}
+
+ES_t GIE_enuDisable(void){
+    // Disable Global Interrupt Instruction
+    asm("CLI");
+    return ES_OK;
+}

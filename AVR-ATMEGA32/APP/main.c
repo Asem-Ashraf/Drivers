d6529c081a4ce79a458a6c77f8e3031e642442de
Asem Ashraf <asem.ashraf18@gmail.com>
2023-04-25 02:43:09 +0200

    Added APP layer

---
+++ AVR-ATMEGA32/APP/main.c
@@ -0,0 +1,19 @@
+#include "../LIB/Bit_Math.h"
+#include "../LIB/stdTypes.h"
+#include "../LIB/ErrorStates.h"
+
+#include "../MCAL/DIO/DIO_INTERFACE.h"
+#include "../MCAL/EXTI/EXTI_INTERFACE.h"
+#include "../MCAL/GIE/GIE_INTERFACE.h"
+#include "../MCAL/ADC/ADC_INTERFACE.h"
+
+#include "../HAL/LCD/LCD_INTERFACE.h"
+#include "../HAL/LED/LED_INTERFACE.h"
+#include "../HAL/SWITCH/SW_INTERFACE.h"
+#include "../HAL/7_SEG/SEVEN_SEG_INTERFACE.h"
+
+
+int main() {
+
+    return 0;
+}

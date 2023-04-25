723e8cfa2b0aa063986244b4b13244c2ca024c6e
Asem Ashraf <asem.ashraf18@gmail.com>
2023-04-25 02:02:48 +0200

    Removed includes in interface.h

--- AVR-ATMEGA32/MCAL/GIE/GIE_INTERFACE.h
+++ AVR-ATMEGA32/MCAL/GIE/GIE_INTERFACE.h
@@ -1,10 +1,8 @@
 #ifndef GIE_INTERFACE_H
 #define GIE_INTERFACE_H
 
-#include "../../LIB/stdTypes.h"
-#include "../../LIB/ErrorStates.h"
-
 ES_t GIE_enuEnable(void);
+
 ES_t GIE_enuDisable(void);
 
 #endif // !GIE_INTERFACE_H

0f4bec917604e3fc80139b57b72cf749799df1ed
Asem Ashraf <asem.ashraf18@gmail.com>
2023-06-17 20:20:43 +0300

    reduced devices

--- AVR-ATMEGA32/HAL/LCD/LCD_CONFIG.c
+++ AVR-ATMEGA32/HAL/LCD/LCD_CONFIG.c
@@ -76,29 +76,11 @@ typedef struct {
     u8 LCD_u8DB7Pin;
 } LCD_t;
 
-#define LCD_u8DISPLAY_NUM 2
+#define LCD_u8DISPLAY_NUM 1
 
 u8 LCD_u8DisplayNum = LCD_u8DISPLAY_NUM;
 LCD_t LCD_AstructDisplays[LCD_u8DISPLAY_NUM] = {
-    {
-        LCD_u8MODE_8BIT,
-        LCD_u8CURSOR_ON,LCD_u8CURSOR_BLINK_ON,
-        LCD_u8LINES_2,
-        LCD_u8BLOCK_SIZE_5x7,
-        DIO_u8PORTB,DIO_u8PIN0,
-        DIO_u8PORTB,DIO_u8PIN1,
-        DIO_u8PORTB,DIO_u8PIN2,
-
-        DIO_u8PORTA,DIO_u8PIN0,
-        DIO_u8PORTA,DIO_u8PIN1,
-        DIO_u8PORTA,DIO_u8PIN2,
-        DIO_u8PORTA,DIO_u8PIN3,
-        DIO_u8PORTA,DIO_u8PIN4,
-        DIO_u8PORTA,DIO_u8PIN5,
-        DIO_u8PORTA,DIO_u8PIN6,
-        DIO_u8PORTA,DIO_u8PIN7,
-    },
-    {
+        {
         LCD_u8MODE_4BIT,
         LCD_u8CURSOR_OFF,LCD_u8CURSOR_BLINK_OFF,
         LCD_u8LINES_1,

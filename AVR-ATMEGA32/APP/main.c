111ea325a4e5c9b541a7bae24cbc5f5b891c27ef
Asem Ashraf <asem.ashraf18@gmail.com>
2023-06-17 20:16:14 +0300

    LCD_ADC_thermistor test

--- AVR-ATMEGA32/APP/main.c
+++ AVR-ATMEGA32/APP/main.c
@@ -4,83 +4,81 @@
 #include "../LIB/ErrorStates.h"
 
 #include "../MCAL/DIO/DIO_INTERFACE.h"
-#include "../MCAL/GIE/GIE_INTERFACE.h"
 #include "../MCAL/ADC/ADC_INTERFACE.h"
-#include "../MCAL/EXTI/EXTI_INTERFACE.h"
 
-#include "../HAL/LCD/LCD_INTERFACE.h"
 #include "../HAL/LED/LED_INTERFACE.h"
-#include "../HAL/SWITCH/SW_INTERFACE.h"
-#include "../HAL/7_SEG/SEVEN_SEG_INTERFACE.h"
+#include "../HAL/LCD/LCD_INTERFACE.h"
+#include "../HAL/Thermistor/TH_INTERFACE.h"
 
 extern LED_t LED_AstructLed[];
-ES_t gerror=ES_OK;
+extern TH_t TH_AstuThemistors[];
+extern LCD_t LCD_AstructDisplays[];
 
-u16 ADC_u16Result = 0;  
-u8 ADC_u8Result = 0;  
+void floatToString(float num, char* str, int precision) {
+    int integerPart = (int)num;
+    float decimalPart = num - (float)integerPart;
+    int i = 0;
 
-void callback(void) {
-    gerror = ADC_enuGetHighValue(&ADC_u8Result);
-    if (gerror != ES_OK) LED_enuTurnOn(&LED_AstructLed[0]);
-    gerror = DIO_enuSetPortValue(DIO_u8PORTC,ADC_u8Result);
-    if (gerror != ES_OK) LED_enuTurnOn(&LED_AstructLed[0]);
+    // Convert integer part to string
+    if (integerPart == 0) {
+        str[i++] = '0';
+    }
+    else {
+        int temp = integerPart;
+        while (temp != 0) {
+            int digit = temp % 10;
+            str[i++] = digit + '0';
+            temp /= 10;
+        }
+        int len = i;
+        for (int j = 0; j < len / 2; j++) {
+            char temp = str[j];
+            str[j] = str[len - j - 1];
+            str[len - j - 1] = temp;
+        }
+    }
+
+    // Add decimal point and convert decimal part to string
+    if (precision != 0) {
+        str[i++] = '.';
+        for (int j = 0; j < precision; j++) {
+            decimalPart *= 10;
+            int digit = (int)decimalPart;
+            str[i++] = digit + '0';
+            decimalPart -= (float)digit;
+        }
+    }
+
+    // Add null terminator
+    str[i] = '\0';
 }
+
 int main() {
     ES_t error = ES_NOK;
-    error = DIO_enuInit();
-    if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
-
     error = LED_enuInit(LED_AstructLed);
-    if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
-
-    error = ADC_enuInit(ADC_u8PRESCALER_128, ADC_u8LEFT_ADJ,ADC_u8AVCC);
-    if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
-
-    error = ADC_enuSetChannel(ADC_u8CH0);
-    if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
-
-    error = DIO_enuSetPortDirection(DIO_u8PORTC,DIO_u8OUTPUT);
-    if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
-
-    error = ADC_enuSetTrigger(ADC_u8FREE_RUNNING);
-    if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
-
-    error = ADC_enuStartOneConversion();
-    if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
-
-    error = ADC_enuSetCallBack(&callback);
-    if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
-
-    error = ADC_enuEnableInterrupt();
-    if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
-
-    error = GIE_enuEnable();
-    if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
-
-    _delay_ms(5000);
-    error = ADC_enuDisableTrigger();
-    if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
-    _delay_ms(5000);
-    error = ADC_enuStartOneConversion();
-    if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
-    _delay_ms(5000);
-    error = ADC_enuDisableInterrupt();
-    if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
-    error = ADC_enuStartOneConversion();
-    if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
-    error = DIO_enuSetPortValue(DIO_u8PORTC,0);
-    if (error != ES_OK) LED_enuTurnOn(&LED_AstructLed[0]);
-    _delay_ms(5000);
-    error = ADC_enuGetHighValue(&ADC_u8Result);
-    if (error != ES_OK) LED_enuTurnOn(&LED_AstructLed[0]);
-    error = DIO_enuSetPortValue(DIO_u8PORTC,ADC_u8Result);
-    if (error != ES_OK) LED_enuTurnOn(&LED_AstructLed[0]);
-
+        if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
+    error = DIO_enuInit();
+        if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
+    error = ADC_enuInit(ADC_u8PRESCALER_128, ADC_u8RIGHT_ADJ,ADC_u8AVCC); 
+        if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
+    error = TH_enuInit(TH_AstuThemistors);
+        if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
+    error = ADC_enuSetChannel(TH_AstuThemistors[0].TH_u8PINID);
+        if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
+    error = LCD_enuInit();
+        if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
+    f32 f32temp;
+    char str[20];
     while (1) {
-    error = ADC_enuGetHighValue(&ADC_u8Result);
-    if (error != ES_OK) LED_enuTurnOn(&LED_AstructLed[0]);
-    error = DIO_enuSetPortValue(DIO_u8PORTC,ADC_u8Result);
-    if (error != ES_OK) LED_enuTurnOn(&LED_AstructLed[0]);
-
+        error = TH_enuGetTemp(&TH_AstuThemistors[0], &f32temp);
+            if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
+        floatToString(f32temp, str,8);
+        for (int i =0; i<6; i++) {
+            error = LCD_enuWriteChar(&LCD_AstructDisplays[0], str[i]);
+                if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
+        }
+        _delay_ms(3000);
+        error = LCD_enuClear(&LCD_AstructDisplays[0]);
+            if (error != ES_OK)  { LED_enuTurnOn(&LED_AstructLed[0]); return 0; }
     }
 }

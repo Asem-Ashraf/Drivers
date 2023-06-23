8b5cce738315ea9a3fccfa5fb157cf0d34f70e8d
Asem Ashraf <asem.ashraf18@gmail.com>
2023-06-23 16:08:32 +0300

    Removed volatile from array declaration

--- AVR-ATMEGA32/MCAL/EXTI/EXTI_PROGRAM.c
+++ AVR-ATMEGA32/MCAL/EXTI/EXTI_PROGRAM.c
@@ -26,7 +26,7 @@
 #define RISING_EDGE    0b00000011
 
 
-static volatile void (*EXTI_AFptrCallback[3])(void)={NULL,NULL,NULL};
+static void (*EXTI_AFptrCallback[3])(void)={NULL,NULL,NULL};
 
 ES_t EXTI_enuInitInterrupt(u8 EXTI_u8InterruptID, void (*EXTI_FptrCallback)(void)){
     if (EXTI_u8InterruptID>2)           return ES_OUT_OF_RANGE;

0961719bc8dacd962d36612af6c22db8b27a84a1
Asem Ashraf <asem.ashraf18@gmail.com>
2023-06-26 23:46:27 +0300

    Added documentation and removed source file of GIE driver

--- AVR-ATMEGA32/MCAL/GIE/GIE_INTERFACE.h
+++ AVR-ATMEGA32/MCAL/GIE/GIE_INTERFACE.h
@@ -1,8 +1,32 @@
+/**
+ * @file GIE_interface.h
+ * @brief This file contains the interface for the General Interrupt Enable 
+ * (GIE) driver.
+ *        The GIE driver provides functions for enabling and disabling 
+ *        interrupts on the AVR microcontroller.
+ */
+
 #ifndef GIE_INTERFACE_H
 #define GIE_INTERFACE_H
 
+/**
+ * @brief Enables global interrupts on the AVR microcontroller.
+ *        This function enables global interrupts on the AVR microcontroller by
+ *        setting the Global Interrupt Enable (GIE) bit in the Status Register 
+ *        (SREG).
+ * @param None.
+ * @return None.
+ */
 #define GIE_Enable() asm("SEI")
 
+/**
+ * @brief Disables global interrupts on the AVR microcontroller.
+ *        This function disables global interrupts on the AVR microcontroller by
+ *        clearing the Global Interrupt Enable (GIE) bit in the Status Register
+ *        (SREG).
+ * @param None.
+ * @return None.
+ */
 #define GIE_Disable() asm("CLI")
 
 #endif // !GIE_INTERFACE_H
--- AVR-ATMEGA32/MCAL/GIE/GIE_PROGRAM.c
+++
@@ -1,6 +0,0 @@
-// void GIE_Enable(){
-//     asm("SEI"); // Enable Global Interrupt Instruction
-// }
-// void GIE_Disable(){
-//     asm("CLI"); // Disable Global Interrupt Instruction
-// }
